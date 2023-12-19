638816a52a31e13c242662dede21638a349f32da
A-Kija <arvydas.kijakauskas@gmail.com>
2023-12-19 15:29:57 +0200

    012

--- 012/index.php
+++ 012/index.php
@@ -17,6 +17,10 @@
 
     print_r($_GET);
 
+    print_r($_POST);
+
+    print_r($_SERVER['REQUEST_METHOD']);
+
     if (($_GET['kas'] ?? '') == 'kazkas') {
         echo '<h2>Labas, kazkas!</h2>';
     }
@@ -31,6 +35,13 @@
         <input type="color" name="kitas">
         <input type="hidden" name="a" value="5">
         <button type="submit">GET IT</button>
+    </form>
+
+    <form action="http://localhost/capybaros/012/?z=88" method="post">
+        <input type="text" name="kas">
+        <input type="color" name="kitas">
+        <input type="hidden" name="a" value="5">
+        <button type="submit">POST IT</button>
     </form>
     
     </pre>
