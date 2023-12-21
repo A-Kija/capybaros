0970ccbf9d8c6adda4f2bf55378a251df145ae5b
A-Kija <arvydas.kijakauskas@gmail.com>
2023-12-21 14:15:30 +0200

    014A

--- auth/authorized.php
+++ auth/authorized.php
@@ -6,11 +6,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Forest</title>
+    <title>Inside the Forest</title>
 </head>
 <body>
-    <h1>Welcome to Forest</h1>
-    <a href="login.php">Login</a> or <a href="register.php">Register</a>
+    <h1>Welcome to Forest members page</h1>
+    <h2>Hello, <?= $_SESSION['name'] ?></h2>
+
     
 </body>
 </html>
