736f137a9a840ace76f84e03017b57a52a07b990
A-Kija <arvydas.kijakauskas@gmail.com>
2023-12-28 14:16:08 +0200

    up

--- crud/create.php
+++ crud/create.php
@@ -1,9 +1,11 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="http://localhost/capybaros/crud/app.js" defer></script>
     <title>Create</title>
 </head>
 <body>
@@ -18,8 +20,8 @@
                     <div class="card-body">
                         <form action="http://localhost/capybaros/crud/store.php" method="post">
                             <div class="mb-3">
-                                <label class="form-label">Amount:</label>
-                                <input type="range" class="form-range" name="amount" min="0" max="1000">
+                                <label class="form-label">Amount: <span id="amount"></span></label>
+                                <input type="range" class="form-range" name="amount" min="0" max="1000" value="10">
                             </div>
                             <button type="submit" class="btn btn-outline-primary">Create</button>
                         </form>                            
--- crud/read.php
+++ crud/read.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,6 +49,8 @@
                     </div>
                     <div class="col-8">
                         <a href="http://localhost/capybaros/crud/show.php?id=<?= $box['boxId'] ?>" class="btn btn-outline-success btn-sm">Show</a>
+                        <a href="http://localhost/capybaros/crud/edit.php?id=<?= $box['boxId'] ?>" class="btn btn-outline-info btn-sm">Edit</a>
+                        <a href="http://localhost/capybaros/crud/delete.php?id=<?= $box['boxId'] ?>" class="btn btn-outline-danger btn-sm">Delete</a>
                     </div>
                 </div>
             </div>
