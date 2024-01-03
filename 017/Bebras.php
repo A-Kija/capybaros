cf4700e7f7ed544ebad853b4db3deb9837e05978
A-Kija <arvydas.kijakauskas@gmail.com>
2024-01-03 19:18:56 +0200

    000

--- 017/Bebras.php
+++ 017/Bebras.php
@@ -4,7 +4,38 @@ class Bebras {
 
     public $spalva = 'ruda';
     private $svoris = 'nezinomas'; // dar nepasverem
-    private $ugis = 1.5; // metrais
+    private $ugis = 1.0; // metrais
+
+
+
+    public function __construct() {
+        echo '<br>Bebras atėjo<br>';
+    }
+
+    public function __destruct() {
+        echo '<br>Bebras išėjo<br>';
+    }
+
+    public function __toString(): string {
+        return "<br>Bebras spalvos: $this->spalva, ugis: $this->ugis, svoris: $this->svoris<br>";
+    }
+
+    public function __invoke() {
+        echo '<br>Bebras sako:<br>';
+        echo 'Labas<br>';
+    }
+
+    public function __serialize(): array {
+        return [
+            'ugis' => $this->ugis,
+            'svoris' => $this->svoris
+        ];
+    }
+
+    public function __unserialize(array $data): void {
+        $this->ugis = $data['ugis'] * 2;
+        $this->svoris = $data['svoris'];
+    }
 
     public function __get($prop) {
 
@@ -15,6 +46,24 @@ class Bebras {
             default => null
         };
         
+    }
+
+    public function __set($prop, $val) {
+
+        if ($prop == 'ugis') {
+            if ($val < 0.8 || $val > 1.0) {
+                echo 'Blogai ivestas ugis';
+                return;
+            }
+            $this->ugis = $val;
+        }
+        // if ($prop == 'svoris') {
+        //     if ($val < 5 || $val > 45) {
+        //         echo 'Blogai ivestas svoris';
+        //         return;
+        //     }
+        //     $this->svoris = $val;
+        // }
     }
 
     private function kokiaUodega() {
