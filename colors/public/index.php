f9fdf38a5a2bb9a93ce4b497a8343d7044da631f
A-Kija <arvydas.kijakauskas@gmail.com>
2024-01-10 15:32:21 +0200

    C2

--- colors/public/index.php
+++ colors/public/index.php
@@ -1,4 +1,9 @@
 <?php
+use Colors\App\App;
+
+require '../vendor/autoload.php';
+define('ROOT', __DIR__ . '/../');
+define('URL', 'http://super-colors.test');
 
 
-echo '<br> -- Colors -- <br>';
+echo App::run();
